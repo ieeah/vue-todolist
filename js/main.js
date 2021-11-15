c52ee0f847d7d2cd6c7a98e3a57956a75a716a3a
ieeah <lbnfnc90@gmail.com>
2021-11-15 15:37:53 +0100

    done input binding

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
+// generare layout statico aggiunta todo
+// generare dinamicamente x li, uno per ogni todo incluso nell'array di oggetti, contenete il suo corpo e un'icona per la cancellazione del todo stesso
+// AGGIUNTA NUOVI TOdo
+// quando si clicca sul bottone aggiungi, il valore all'interno del campo di input, viene inserito come testo di un nuovo oggetto nella lista todos (il campo completed di default è sempre false)
+// lasciare il focus sul campo di input (usare $refs)
+// aggiungere l'azione quando si preme il tasto enter
+
+
+
 
 const app = new Vue({
     el: '#app',
+    data: {
+        todos: [
+            {
+                text: 'corpo todo numero 1',
+                completed: true,
+            },
+            {
+                text: 'corpo todo numero 2',
+                completed: true,
+            },
+            {
+                text: 'corpo todo numero 3',
+                completed: false,
+            },
+        ],
+        newTodoText: '',
+    },
+    methods: {
+        addTodo() {
+            this.todos.push({
+                text: this.newTodoText,
+                completed: false,
+            },);
+        },
+    },
 });
-
